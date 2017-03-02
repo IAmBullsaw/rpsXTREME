e9586b9f6de52ac9e503d999318ca639befd6a08
Oskar Jansson <oskja067@student.liu.se>
2017-03-02 18:23:14 +0100

    Moved actual game into game folder

--- client/client.py
+++ client/client.py
@@ -4,7 +4,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '155.4.151.254'
 port = 4711
 
-print("Connect")
+print("Connecting to {}:{}".format(host,port))
 sock.connect((host,port))
 print("Recv")
 msg = sock.recv(1024)
