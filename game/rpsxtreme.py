81a582805ae2810e579d97d6194173aec593330d
Oskar Jansson <oskja067@student.liu.se>
2017-03-03 09:54:05 +0100

    Evolving game motor to a class

--- game/rpsxtreme.py
+++ game/rpsxtreme.py
@@ -8,6 +8,74 @@ from term_gfx import Graphics
 
 debug = False
 
+class RPSXGame:
+
+    def __init__(self, p1, p2, judge):
+        self.p1 = p1
+        self.p2 = p2
+        self.judge = judge
+        self.stats = Statistics(p1, p2)
+    
+    def get_player_choice(self, player):
+        done = False
+        choice = -1
+        while not done:
+            choice = input('What is your move?: ')
+            if choice in ['q','quit','exit']:
+                done = True
+                choice = -1
+            if choice in '123':
+                choice = int(choice)
+                choice -= 1 # minus one since choices do not begin with 0
+                choice = choice % 3
+                if player.can_choose_move(choice):
+                    done = True
+        return choice
+
+    def play(self):
+        gfx = Graphics()
+
+        gfx.battling_players(self.p1, self.p2)
+        self.judge.on_start_of_match(self.p1, self.p2)
+        for i in range(5):
+            #judge.on_start_of_turn(p1,p2)
+        
+            # If a player has no more moves left match is over
+            if not self.p1.has_moves_left() or not self.p2.has_moves_left():
+                gfx.show_stats(self.stats)
+                gfx.show_winner(self.stats)
+                return
+
+            # Begin turn and request choices
+        
+            gfx.show_stats(self.stats)
+            gfx.show_moves(self.p1.get_moves())
+            p1_choice = self.p1.get_chosen_move()#  get_player_choice(self.p1)
+
+            # if choice is -1, we wanted to exit.
+            if p1_choice == -1:
+                gfx.show_stats(self.stats)
+                gfx.goodbye_screen()
+                return
+        
+        
+            p2_choice = self.judge.get_random_choice(self.p2)
+        
+            # Get outcome
+            winner, loser, winner_move, loser_move = self.judge.determine_match_outcome(self.p1,
+                                                                                        p1_choice,
+                                                                                        self.p2,
+                                                                                        p2_choice)
+            if (winner != None):
+                self.judge.determine_match_fallout(winner, loser, winner_move, loser_move)
+            else:
+                self.judge.determine_draw_fallout(self.p1, self.p2, p1_choice, p2_choice)
+        gfx.show_stats(self.stats)
+        gfx.show_winner(self.stats)
+    
+################
+# Old colde -_-
+"""
 def show_moves(player):
     print("{}'s moves:\n1. Rocks: {}\n2. Paper: {}\n3. Scissors: {}\n".format(player.name,player.rocks,player.papers,player.scissors))
             
@@ -95,3 +163,4 @@ if __name__ == '__main__':
         answer = input('Are you done kicking ass? (y/n)')
         if answer in ['yes','yeah','yup','y']:
             done = True
+"""
