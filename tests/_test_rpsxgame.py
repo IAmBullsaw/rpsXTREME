149f5c5d8a94bd005b9a8357e082d3d44557fbe4
Oskar Jansson <oskja067@student.liu.se>
2017-03-03 15:19:09 +0100

    Added more tests for RPSXGame

--- tests/_test_rpsxgame.py
+++ tests/_test_rpsxgame.py
@@ -1,14 +1,30 @@
 import sys
 sys.path.insert(0,'../game')
+sys.path.insert(0,'../server')
+sys.path.insert(0,'../client')
+
 from rpsxtreme import RPSXGame
 from player import Player
 from judge import Judge
+from rpsxserver import RPSXServer
 
-def test():
-    p1 = Player('p1')
+def plp(test):
+    print('\t',test,'\tPassed')
+
+def test_RPSXGame():
+    p1 = Player('p1',bot=True)
     bot = Player('p2',bot=True)
     game = RPSXGame(p1,bot,Judge())
-    game.play()
+    for i in range(10):
+        game.play()
+    plp('test_RPSXGame()')
+
+def test_RPSXServer():
+    plp('test_RPSXServer()')
+    server = RPSXServer('',port = 5000)
+    server.run()
+    pass
     
 if __name__ == '__main__':
-    test()
+    test_RPSXGame()
+    test_RPSXServer()
